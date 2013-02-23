fa2872a103da7a98f6c9a9a4b7d53c19f2a1a11c
xiaobo-hu <xiaobohu@128.237.201.136>
2013-02-23 14:45:15 -0500

    User and Tutorial

--- BioImage/src/objectModel/User.java
+++ BioImage/src/objectModel/User.java
@@ -1,7 +1,41 @@
 package objectModel;
 
 public class User {
+	String name;
+	String address;
+	String email;
+	char[] password;
 	
-	
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public String getName() {
+		return name;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+	
+	public char[] getPassword() {
+		return password;
+	}
+	
 }
