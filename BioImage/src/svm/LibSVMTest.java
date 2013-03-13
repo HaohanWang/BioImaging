50dbb252bd798f54ea9310ec023ae5cb473af0bf
xiaobo-hu <xiaobohu@192.168.1.22>
2013-03-13 02:03:00 -0700

    svm test

--- BioImage/src/svm/LibSVMTest.java
+++ BioImage/src/svm/LibSVMTest.java
@@ -7,26 +7,15 @@ import libsvm.*;
 public class LibSVMTest {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		//Test for svm_train and svm_predict
-		//svm_train: 
-		//	  param: String[], parse result of command line parameter of svm-train
-		//    return: String, the directory of modelFile
-		//svm_predect:
-		//	  param: String[], parse result of command line parameter of svm-predict, including the modelfile
-		//    return: Double, the accuracy of SVM classification
 
 		String[] trainArgs = {"test.txt"};
 		String modelFile = svm_train.main(trainArgs);		
-//		String[] trainArgs = {"UCI-breast-cancer-tra"};//directory of training file
-//		String modelFile = svm_train.main(trainArgs);
-//		String[] testArgs = {"UCI-breast-cancer-test", modelFile, "UCI-breast-cancer-result"};//directory of test file, model file, result file
+//		String[] testArgs = {"test", modelFile, "result"};//directory of test file, model file, result file
 //		Double accuracy = svm_predict.main(testArgs);
-//		System.out.println("SVM Classification is done! The accuracy is " + accuracy);
-		//Test for cross validation
-		//String[] crossValidationTrainArgs = {"-v", "10", "UCI-breast-cancer-tra"};// 10 fold cross validation
-		//modelFile = svm_train.main(crossValidationTrainArgs);
-		//System.out.print("Cross validation is done! The modelFile is " + modelFile);
+//		System.out.println("SVM Classification is done!");
+//		System.out.println("The modelFile is " + modelFile);
+//		System.out.println("The accuracy is " + accuracy);
+
 	}
 
 }
